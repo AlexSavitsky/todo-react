093897858de38ddb8a8ea8658a2bc5f04d0d0060
Alexandr <savasavitsky@gmail.com>
2022-05-22 16:13:32 +0300

    Adding functionality
    -Done task
    -Important task button
    -Delete task button

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./todo-list-item.css";
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-    fontWeight: important ? "bold" : "normal",
+export default class TodoListItem extends Component {
+  state = {
+    done: false,
+    important: false,
   };
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
+  onMarkImportant = () => {
+    this.setState(({ important }) => {
+      return { important: !important };
+    });
+  };
+
+  onLabelClick = () => {
+    this.setState(({ done }) => {
+      return { done: !done };
+    });
+  };
+
+  render() {
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += " done";
+    }
+
+    if (important) {
+      classNames += " important";
+    }
+
+    return (
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger"
+          onClick={onDeleted}
+        >
+          <i className="fa-regular fa-trash-can"></i>
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-success"
+          onClick={this.onMarkImportant}
+        >
+          <i className="fas fa-exclamation"></i>
+        </button>
       </span>
-
-      <button type="button" className="btn btn-outline-danger">
-        <i class="fa-regular fa-trash-can"></i>
-      </button>
-
-      <button type="button" className="btn btn-outline-success">
-        <i class="fas fa-exclamation"></i>
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
